e6c3020d019a6e5fe5572c01ab407d9835313241
PancakeTAS <pancake@mgnet.work>
2025-10-23 13:34:23 +0200

    refactor: reset project

--- include/constants.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstddef>
-
-namespace tun {
-    const size_t RECV_BUF = 65535; //!< receive buffer size
-    const size_t SEND_BUF = 65535; //!< send buffer size
-    const size_t HSLEN = 5; //!< handshake packet length
-}
